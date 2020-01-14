019debdf92d0895f6b821ad0d30a11873ccdf341
zijianzhou <961217812@qq.com>
2020-01-14 17:28:06 +0800

    no message

---
+++ src/components/content/BackTop.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="backtop">
+        <img src="~assets/img/common/top.png"/>
+    </div>
+</template>
+
+<style >
+.backtop {
+    position: fixed;
+    right: 15px;
+    bottom: 55px;
+    width: 43px;
+}
+.backtop img{
+    width: 100%;
+}
+</style>
